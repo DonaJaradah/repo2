5f5636096cf4b366e7ebfde6e9296bf5f9d198b4
DonaJaradah <dona.ramadan@student.guc.edu.eg>
2018-03-02 23:14:45 +0200

    back end added

---
+++ nodejs-seed-project1/api/models/Product.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const productSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: 0
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  sellerName: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true
+  },
+  updatedAt: Date
+});
+
+mongoose.model('Product', productSchema);
